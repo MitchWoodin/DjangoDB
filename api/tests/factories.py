9f61faaff35c0a91ccf1fdbaf8997de9daf4f5f4
Mitchell Woodin <mitchellwoodinart@gmail.com>
2023-12-08 17:47:25 +1100

    adds test coverage for creation of DB items

--- api/tests/factories.py
+++ api/tests/factories.py
@@ -5,7 +5,27 @@ class AssetFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "api.Asset"
 
-    uid = factory.Faker("uuid4")
+    asset_type = factory.Faker("word")
+    created_by = factory.Faker("name")
     path = factory.Faker("file_path")
     name = factory.Faker("file_name")
+
+
+class ReviewFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "api.Review"
+
+    path = factory.Faker("file_path")
+    name = factory.Faker("file_name")
+    shot = factory.Faker("file_name")
     asset_type = factory.Faker("word")
+    created_by = factory.Faker("name")
+
+
+class PackFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "api.Pack"
+
+    name = factory.Faker("file_name")
+    pack_type = factory.Faker("word")
+    contents = factory.Faker("json")
--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -1,4 +1,4 @@
-from api.tests.factories import AssetFactory
+from api.tests.factories import AssetFactory, ReviewFactory, PackFactory
 
 
 class TestAsset:
@@ -7,7 +7,37 @@ class TestAsset:
 
         assert asset is not None
 
-        assert asset.uid != ""
         assert asset.path != ""
         assert asset.name != ""
         assert asset.asset_type != ""
+        assert asset.created_by != ""
+
+        # TODO: Test items not in Factory
+
+
+class TestReview:
+    def test_factory(self):
+        review = ReviewFactory()
+
+        assert review is not None
+
+        assert review.path != ""
+        assert review.name != ""
+        assert review.asset_type != ""
+        assert review.created_by != ""
+        assert review.shot != ""
+
+        # TODO: Test items not in Factory
+
+
+class TestPack:
+    def test_factory(self):
+        pack = PackFactory()
+
+        assert pack is not None
+
+        assert pack.name != ""
+        assert pack.pack_type != ""
+        assert pack.contents != ""
+
+        # TODO: Test items not in Factory
